2bec6f76729e48409921f464f95806a844c51cb3
Vektor <zeltobryxov@mail.ru>
2025-06-22 18:30:02 +0300

    Попытка улучишь сохранение доков

--- widget/script.js
+++ widget/script.js
@@ -140,3 +140,135 @@ define(['jquery'], function ($) {
 
   return CustomWidget;
 });
+
+// Попытался улучшить сохранение поскольку боялся что то испорить то заделал всё в комент
+/*define(['jquery'], function ($) {
+  var CustomWidget = function () {
+    var self = this;
+
+    this.callbacks = {
+      render: function () {
+        var settings = self.get_settings();
+
+        var observer = new MutationObserver(function () {
+          var $target = $('.card-fields__top-name-field');
+          if ($target.length && $('.button-input.generate-doc').length === 0) {
+            var $buttonGoogle = $('<button class="button-input generate-doc">Создать документ в Google</button>');
+            var $buttonYandex = $('<button class="button-input generate-doc-yandex">Создать документ в Яндекс</button>');
+            $target.after($buttonGoogle);
+            $target.after($buttonYandex);
+
+            $buttonGoogle.on('click', function () {
+              var lead = AMOCRM.data.current_card;
+              self.generateDocumentGoogle(lead);
+            });
+
+            $buttonYandex.on('click', function () {
+              var lead = AMOCRM.data.current_card;
+              self.generateDocumentYandex(lead);
+            });
+
+            observer.disconnect();
+          }
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+
+        return true;
+      },
+
+      init: function () {
+        return true;
+      },
+
+      settings: function () {
+        return self.render({
+          href: '/templates/settings.twig',
+          base_path: self.params.path,
+          load: function (template) {
+            var $content = $(template.render({
+              google_url: self.get_settings().google_url,
+              yandex_url: self.get_settings().yandex_url,
+              secret: self.get_settings().secret
+            }));
+
+            return $content;
+          }
+        });
+      },
+
+      onSave: function () {
+        var googleUrl = $('input[name="google_url"]').val();
+        var yandexUrl = $('input[name="yandex_url"]').val();
+
+        if (!googleUrl && !yandexUrl) {
+          return false;
+        }
+
+        return {
+          google_url: googleUrl,
+          yandex_url: yandexUrl
+        };
+      }
+    };
+
+    this.save_google_url = function (url) {
+      if (!url) {
+        AMOCRM.notifications.show_message({
+          text: 'Введите URL Google Apps Script',
+          type: 'error'
+        });
+        return;
+      }
+
+      AMOCRM.widgets.save({
+        google_url: url
+      }).then(function () {
+        AMOCRM.notifications.show_message({
+          text: 'URL успешно сохранен',
+          type: 'success'
+        });
+      }).catch(function () {
+        AMOCRM.notifications.show_message({
+          text: 'Ошибка при сохранении URL',
+          type: 'error'
+        });
+      });
+    };
+
+    this.save_yandex_url = function (url) {
+      if (!url) {
+        AMOCRM.notifications.show_message({
+          text: 'Введите URL Яндекс Документов',
+          type: 'error'
+        });
+        return;
+      }
+
+      AMOCRM.widgets.save({
+        yandex_url: url
+      }).then(function () {
+        AMOCRM.notifications.show_message({
+          text: 'URL успешно сохранен',
+          type: 'success'
+        });
+      }).catch(function () {
+        AMOCRM.notifications.show_message({
+          text: 'Ошибка при сохранении URL',
+          type: 'error'
+        });
+      });
+    };
+
+    this.generateDocumentGoogle = function (lead) {
+      var settings = self.get_settings();
+
+      if (!settings.google_url) {
+        AMOCRM.notifications.show_message({
+          text: 'URL Google Apps Script не настроен',
+          type: 'error'
+        });
+        return;
+      }
+
+      var $button = $('.button-input').text('Генерация...');
